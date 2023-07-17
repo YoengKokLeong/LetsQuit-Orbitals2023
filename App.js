9688843228d90853fe75e53bea548197323cbd23
YoengKokLeong <98523851+YoengKokLeong@users.noreply.github.com>
2023-07-18 01:00:03 +0800

    Updated App.js
    added Profile and Help route path

--- App.js
+++ App.js
@@ -1,26 +1,27 @@
-import './App.css';
-import {useState} from "react";
-import StreakManager from "./components/StreakManager.js";
-import InputName from "./components/InputName";
-import InputAddiction from "./components/InputAddiction";
-import AddCalendar from "./components/AddCalendar";
+import Home from "./Home";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
-import AuthDetails from './components/AuthDetails';
+import Shop from "./components/Shop";
+import Social from "./components/Social";
+import Profile from "./components/Profile";
+import Help from "./components/Profile";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
-const [tasks,setTasks] = useState([]);
 
   return (
     <div className="App">
-       <SignIn />
-       <SignUp />
-       <AuthDetails />
-       <InputName numOfTasks ={tasks.length}/>
-       <InputAddiction />
-       <StreakManager tasks={tasks} setTasks = {setTasks} />
-       <AddCalendar />
+      <Routes>
+      <Route path = "/" element = {<SignIn />}> </Route>
+      <Route path = "/SignUp" element = {<SignUp />}> </Route>
+      <Route path = "/Home" element = {<Home />}> </Route>
+      <Route path = "/Shop" element = {<Shop />}> </Route>
+      <Route path = "/Social" element = {<Social />}> </Route>
+      <Route path = "/Profile" element = {<Profile />}> </Route>
+      <Route path = "/Help" element = {<Help />}> </Route>
+      </Routes>
   </div>
+
   );
 }
 
