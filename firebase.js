70b8a072cc0ace79b916875cca6e7cd2c3effea0
YoengKokLeong <98523851+YoengKokLeong@users.noreply.github.com>
2023-07-18 01:05:51 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import {getFirestore} from "@firebase/firestore"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+export const auth = getAuth(app); // auth feature
+export const db = getFirestore(app); //real time database
+export const storage = getStorage(app); //photo storage
