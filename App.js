93f5c907fbc71565fedf2e3576e6bfad57a45b09
YoengKokLeong <98523851+YoengKokLeong@users.noreply.github.com>
2023-07-25 17:33:58 +0800

    Ensured consistent formatting

--- App.js
+++ App.js
@@ -4,24 +4,40 @@ import SignUp from "./components/auth/SignUp";
 import Shop from "./components/Shop";
 import Social from "./components/Social";
 import Profile from "./components/Profile";
-import Help from "./components/Profile";
-import {Routes, Route} from "react-router-dom";
+import Help from "./components/Help";
+import Inventory from "./components/Inventory";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <div className="App">
       <Routes>
-      <Route path = "/" element = {<SignIn />}> </Route>
-      <Route path = "/SignUp" element = {<SignUp />}> </Route>
-      <Route path = "/Home" element = {<Home />}> </Route>
-      <Route path = "/Shop" element = {<Shop />}> </Route>
-      <Route path = "/Social" element = {<Social />}> </Route>
-      <Route path = "/Profile" element = {<Profile />}> </Route>
-      <Route path = "/Help" element = {<Help />}> </Route>
+        <Route path="/" element={<SignIn />}>
+          {" "}
+        </Route>
+        <Route path="/SignUp" element={<SignUp />}>
+          {" "}
+        </Route>
+        <Route path="/Home" element={<Home />}>
+          {" "}
+        </Route>
+        <Route path="/Shop" element={<Shop />}>
+          {" "}
+        </Route>
+        <Route path="/Social" element={<Social />}>
+          {" "}
+        </Route>
+        <Route path="/Profile" element={<Profile />}>
+          {" "}
+        </Route>
+        <Route path="/Help" element={<Help />}>
+          {" "}
+        </Route>
+        <Route path="/Inventory" element={<Inventory />}>
+          {" "}
+        </Route>
       </Routes>
-  </div>
-
+    </div>
   );
 }
 
